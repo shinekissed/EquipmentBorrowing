11d26b485e7ab0d20a20701bf5ab6cdbc65a1339
Airesh Shine Abcede <abcede.aireshine@gmail.com>
2026-09-11 00:06:02 +0800

    Add equipment view and bindings

--- src/EquipmentBorrowing.Desktop/ViewModels/EquipmentViewModel.cs
+++ src/EquipmentBorrowing.Desktop/ViewModels/EquipmentViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace EquipmentBorrowing.Desktop.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Desktop.ViewModels;
 
 public partial class EquipmentViewModel : ViewModelBase
 {
-    public string Title => "Equipment Catalog & Borrowing";
+    public ObservableCollection<Equipment> EquipmentList { get; } = new();
+
+    [ObservableProperty]
+    private Equipment? _selectedEquipment;
+
+    public EquipmentViewModel()
+    {
+        LoadSeedData();
+    }
+
+    private void LoadSeedData()
+    {
+        EquipmentList.Add(new Equipment(100, "Digital Multimeter", isAvailable: true));
+        EquipmentList.Add(new Equipment(101, "Digital Oscilloscope", isAvailable: false));
+        EquipmentList.Add(new Equipment(102, "Soldering Station Kit", isAvailable: true));
+        EquipmentList.Add(new Equipment(103, "DC Power Supply (30V/5A)", isAvailable: true));
+        EquipmentList.Add(new Equipment(104, "Function Generator", isAvailable: false));
+
+        // Select first item by default
+        SelectedEquipment = EquipmentList[0];
+    }
 }
